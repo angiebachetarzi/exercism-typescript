bbf09a1aeff2f1e0f0149fb17b5507dbd920e5f5
Angie Bachetarzi <angie.bachetarzi@creamconsulting.com>
2024-02-02 22:54:00 +0100

    rational-numbers: all tests pass

--- rational-numbers/rational-numbers.ts
+++ rational-numbers/rational-numbers.ts
@@ -1,37 +1,67 @@
 export class Rational {
-  constructor() {
-    throw new Error('Remove this statement and implement this function')
+  public numerator: number;
+  public denominator: number;
+  constructor(n: number, d: number) {
+    this.numerator = n;
+    this.denominator = d;
   }
 
-  add() {
-    throw new Error('Remove this statement and implement this function')
+  add(r: Rational): Rational {
+    let res = new Rational(0,0);
+    if (this.denominator != r.denominator) {
+      res.numerator = (this.numerator * r.denominator) + (r.numerator * this.denominator);
+      res.denominator = this.denominator * r.denominator;
+    } else {
+      res.numerator = this.numerator + r.numerator;
+      res.denominator = this.denominator;
+    }
+    return res.reduce();
   }
 
-  sub() {
-    throw new Error('Remove this statement and implement this function')
+  sub(r: Rational): Rational {
+    return this.add(new Rational(r.numerator * -1, r.denominator));
   }
 
-  mul() {
-    throw new Error('Remove this statement and implement this function')
+  mul(r: Rational): Rational {
+    return new Rational(this.numerator * r.numerator, this.denominator * r.denominator).reduce();
   }
 
-  div() {
-    throw new Error('Remove this statement and implement this function')
+  div(r: Rational): Rational {
+    return this.mul(new Rational(r.denominator, r.numerator));
   }
 
-  abs() {
-    throw new Error('Remove this statement and implement this function')
+  abs(): Rational {
+    return new Rational(Math.abs(this.numerator), Math.abs(this.denominator)).reduce();
   }
 
-  exprational() {
-    throw new Error('Remove this statement and implement this function')
+  exprational(x: number): Rational {
+    if (x < 0) {
+      return new Rational(Math.pow(this.denominator, Math.abs(x)), Math.pow(this.numerator, Math.abs(x))).reduce();
+    } else {
+      return new Rational(Math.pow(this.numerator, Math.abs(x)), Math.pow(this.denominator, Math.abs(x))).reduce();
+    }
   }
 
-  expreal() {
-    throw new Error('Remove this statement and implement this function')
+  expreal(x: number): number {
+    return this.root(Math.pow(x, this.numerator), this.denominator);
   }
 
-  reduce() {
-    throw new Error('Remove this statement and implement this function')
+  //qth root of p
+  root(p: number, q: number): number {
+    return Math.pow(p, 1/q);
+  }
+
+  reduce(): Rational {
+    let gcd = this.getGcd(this.numerator, this.denominator);
+    if (this.denominator/gcd < 0) {
+      return new Rational(this.numerator/gcd * -1, this.denominator/gcd * -1);
+    } else {
+      return new Rational(this.numerator/gcd, this.denominator/gcd);
+    }
+  }
+
+  //get greatest common denominator
+  getGcd(a: number, b: number): number {
+    return b ? this.getGcd(b, a%b) : a;
   }
 }
